61ac933e080748ddeaebf4850f2a837c89856364
MichalRumiancew <michalr11@gmail.com>
2022-12-03 16:07:32 +0100

    task 5

---
+++ src/W3School/task5.java
@@ -0,0 +1,15 @@
+package W3School;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class task5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = a*b;
+        System.out.println(c);
+    }
+}
