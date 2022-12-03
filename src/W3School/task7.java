ce70ed25239bc3cb0530be0c03165734e5197779
MichalRumiancew <michalr11@gmail.com>
2022-12-03 16:26:22 +0100

    task 6

---
+++ src/W3School/task7.java
@@ -0,0 +1,16 @@
+package W3School;
+
+import java.util.Scanner;
+
+public class task7 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+
+        for (int i = 1; i <= 10; i++) {
+            int multiplication = a * i;
+            System.out.println(a + " x " + i + " = " + multiplication);
+        }
+    }
+}
---
+++ src/W3School/task8.java
@@ -0,0 +1,11 @@
+package W3School;
+
+public class task8 {
+    public static void main(String[] args) {
+        System.out.println("   J    a   v     v  a                                                  \n" +
+                "   J   a a   v   v  a a                                                 \n" +
+                "J  J  aaaaa   V V  aaaaa                                                \n" +
+                " JJ  a     a   V  a     a");
+
+    }
+}
