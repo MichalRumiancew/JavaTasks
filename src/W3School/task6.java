4f435783cadad15bdcc1b4f53fb0f9d7249d4b7a
MichalRumiancew <michalr11@gmail.com>
2022-12-03 16:17:23 +0100

    task 6

---
+++ src/W3School/task6.java
@@ -0,0 +1,25 @@
+package W3School;
+
+import java.util.Scanner;
+
+public class task6 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int sum = a + b;
+        int minus = a - b;
+        int multiplication = a * b;
+        int divide = a / b;
+        int mod = a % b;
+
+        System.out.println(a + " + " + b + " = " + sum);
+        System.out.println(a + " - " + b + " = " +minus);
+        System.out.println(a + " x" + b + " = " +multiplication);
+        System.out.println(a + " / " + b + " = " +divide);
+        System.out.println(a + " mod " + b + " = " +mod);
+    }
+}
+
