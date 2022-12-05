92f1a81ec7d1f63dfb382a5ea86d76771d373e0c
MichalRumiancew <michalr11@gmail.com>
2022-12-05 20:47:03 +0100

    task 13

---
+++ src/W3School/areaTask13.java
@@ -0,0 +1,17 @@
+package W3School;
+
+import java.awt.geom.Area;
+
+public class areaTask13 {
+    public static void main(String[] args) {
+        double width = 5.6;
+        double height = 8.5;
+
+        double area = width * height;
+        double perimeter = 2 * width + 2 * height;
+
+        System.out.println(width + " * " + height + " = " + area);
+        System.out.println("2 * " + width + " 2 * " + height + " = " + perimeter);
+
+    }
+}
