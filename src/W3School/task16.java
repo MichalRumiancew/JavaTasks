017795c24ceef8f1e3c8978e90230236a53c2861
MichalRumiancew <michalr11@gmail.com>
2022-12-10 18:59:15 +0100

    16

---
+++ src/W3School/task16.java
@@ -0,0 +1,14 @@
+package W3School;
+
+public class task16 {
+
+    public static void main(String[] args) {
+
+        System.out.println(
+                " +\"\"\"\"\"+                                                 \n" +
+                        "[| o o |]                                                \n" +
+                        " |  ^  |                                                 \n" +
+                        " | '-' |                                                 \n" +
+                        " +-----+");
+    }
+}
