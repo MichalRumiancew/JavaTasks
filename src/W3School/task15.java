092e072ef047f0a429bfc1bd1ff7c1de074fd5b6
MichalRumiancew <michalr11@gmail.com>
2022-12-10 18:02:09 +0100

    14 i 15

---
+++ src/W3School/task15.java
@@ -0,0 +1,21 @@
+package W3School;
+
+import java.util.Scanner;
+
+public class task15 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        System.out.println("a = " + a + " b = " + b);
+
+        int temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println("SWAP " + "a = " + a + " b = " + b);
+
+
+    }
+}
